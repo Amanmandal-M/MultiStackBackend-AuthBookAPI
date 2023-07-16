e0ee1c190e56b7ba889729e47dc3c1bc57eb01b4
Amanmandal-M <amanmandal644@gmail.com>
2023-07-17 01:35:32 +0530

    User Controller created successfully

---
+++ Node_Backend/controllers/user_controller.js
@@ -0,0 +1,140 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+// Models Location
+const userModel = require("../models/user_model");
+
+module.exports.registerController = async (req, res) => {
+  try {
+    const { name, email, contactNo, password, role } = req.body;
+    const saltRounds = 10;
+
+    if (
+      name === "" ||
+      email === "" ||
+      contactNo === "" ||
+      password === "" ||
+      role === ""
+    ) {
+      return res.status(400).send({
+        error: "Bad Request",
+        message: "The request is missing required parameters.",
+      });
+    }
+
+    const isPresent = await userModel.findOne({ email });
+
+    if (isPresent) {
+      return res.status(400).send({
+        error: "Bad Request",
+        message: "User already exists",
+      });
+    }
+
+    bcrypt.hash(password, saltRounds, function (err, hash) {
+      if (err) {
+        return res.status(500).send({
+          error: "Internal Server Error",
+          message: "An error occurred while hashing the password.",
+        });
+      }
+
+      const data = new userModel({
+        name,
+        email,
+        contactNo,
+        password: hash,
+        role,
+      });
+
+      data
+        .save()
+        .then(() => {
+          res.status(201).send({
+            message: "User registered successfully",
+            data: {
+              name: data.name,
+              email: data.email,
+              contactNo: data.contactNo,
+              role: data.role,
+            },
+          });
+        })
+        .catch((error) => {
+          res.status(500).send({
+            error: "Internal Server Error",
+            message: "An error occurred while saving the user.",
+          });
+        });
+    });
+  } catch (error) {
+    res.status(500).send({
+      error: "Internal Server Error",
+      message: "An error occurred.",
+    });
+  }
+};
+
+module.exports.loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (email === "" || password === "") {
+      return res.status(400).send({
+        error: "Bad Request",
+        message: "The request is missing required parameters.",
+      });
+    }
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.status(404).send({
+        error: "Not Found",
+        message: "User not found.",
+      });
+    }
+
+    bcrypt.compare(password, user.password, (err, result) => {
+      if (err) {
+        return res.status(500).send({
+          error: "Internal Server Error",
+          message: "An error occurred while comparing passwords.",
+        });
+      }
+
+      if (!result) {
+        return res.status(401).send({
+          error: "Unauthorized",
+          message: "Invalid credentials.",
+        });
+      }
+
+      const payload = {
+        userId: user._id,
+        role: user.role,
+      };
+
+      const token = jwt.sign(payload, process.env.NORMAL_KEY, {
+        expiresIn: "1h",
+      });
+
+      res.status(200).send({
+        message: "Login successful",
+        token: token,
+        user: {
+          userId: user._id,
+          name: user.name,
+          email: user.email,
+          contactNo: user.contactNo,
+          role: user.role,
+        },
+      });
+    });
+  } catch (error) {
+    res.status(500).send({
+      error: "Internal Server Error",
+      message: "An error occurred.",
+    });
+  }
+};
